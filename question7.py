7f981e67deb569109d575aa0c32f37b403054d2e
Srirama Murthy Chellu <156741558+sc23bk@users.noreply.github.com>
2024-01-29 17:39:15 -0500

    Update question7.py

--- question7.py
+++ question7.py
@@ -1,19 +1,17 @@
 def question7_1():
     """
-    answer = 'string'
+    answer = 'non-decreasing'
     """
     return answer
 
 def question7_2():
     """
-    answer = 'string'
+    answer = 'increase/decrease'
     """
     return answer
 
 def question7_3():
     """
-    answer = [(b1, e1), (b2, e2), ..., (bn, en)]
-    The representation of inifinity is 'infinity'. 
-    bi, ei are floats or 'infinity'. 
+    answer = [(0, 2), (3, 4), (5, 'infinity')]
     """
     return answer 
