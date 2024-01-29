d575437ff0a5451b3195b98323550e05aa097073
Srirama Murthy Chellu <156741558+sc23bk@users.noreply.github.com>
2024-01-29 17:40:59 -0500

    Update question9.py

--- question9.py
+++ question9.py
@@ -1,18 +1,22 @@
 def question9_1():
     """
-    answer = string'
+    answer = 'SMC'
     """
     return answer
 
 def question9_2():
+    answer = 'Jaccard'
     return answer
 
 def question9_3():
+    answer = 'correlation coefficient' 
     return answer 
 
 def question9_4():
+    answer = 'Cosine Similarity'
     return answer 
 
 def question9_5():
+    answer = 'Cosine Similarity'
     return answer 
 
