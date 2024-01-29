17e3ad904e61919c53d8d815a68b9ff4e8e6669e
Srirama Murthy Chellu <156741558+sc23bk@users.noreply.github.com>
2024-01-29 18:05:30 -0500

    Update question7.py

--- question7.py
+++ question7.py
@@ -1,11 +1,11 @@
 def question7_1():
-    answer ='increase/decrease'
+    
     return answer
 
 def question7_2():
-    answer = 'non-decreasing'
+    
     return answer
 
 def question7_3()
-    answer = [(0., 4.), (4., 5.), (5., 8.),(8., 'infinity')]
+    
     return answer 
