b3757205836467176fea05dc87af80d404629e93
Srirama Murthy Chellu <156741558+sc23bk@users.noreply.github.com>
2024-01-29 17:57:41 -0500

    Update question6.py

--- question6.py
+++ question6.py
@@ -1,21 +1,21 @@
 def question6_1():
     answer = {
-        'equal_width': ['Change', 0], 
-        'equal_frequency': ['Change',0]
+        'equal_width': ['No Change', 0], 
+        'equal_frequency': ['No Change',0]
     }
     return answer
 
 def question6_2():
     answer = {
-        'equal_width': ['Change', 2],
-        'equal_frequency': ['Change',2 ]
+        'equal_width': ['No Change', 2],
+        'equal_frequency': ['No Change',2 ]
     }
     return answer
 
 def question6_3():
     answer = {
-        'equal_width': ['Change', 2], 
-        'equal_frequency': ['Change',2 ]
+        'equal_width': ['No Change', 2], 
+        'equal_frequency': ['No Change',2 ]
     }
     return answer 
 
