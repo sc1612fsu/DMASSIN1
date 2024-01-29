6e4a832ab3baf2cb64c7a8423f509cc74c7891f8
Srirama Murthy Chellu <156741558+sc23bk@users.noreply.github.com>
2024-01-29 17:43:36 -0500

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,15 +1,19 @@
 def question2_1():
-    # answer = ['string', 'string', 'string']
+    answer = ['Discrete', 'Quantitative', 'Ratio']
     return answer
 
 def question2_2():
+    answer = ['Continuous', 'Quantitative', 'Ratio']
     return answer
 
 def question2_3():
+    answer = ['Continuous', 'Quantitative', 'Interval']
     return answer 
 
 def question2_4():
+    answer = ['Discrete', 'Qualitative', 'Ordinal']
     return answer
 
 def question2_5():
+    answer = ['Binary', 'Qualitative', 'Nominal']
     return answer
