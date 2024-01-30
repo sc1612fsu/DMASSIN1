24c537ed1a2f56e647eeebd26e95fa684732ce7b
Srirama Murthy Chellu <156741558+sc23bk@users.noreply.github.com>
2024-01-29 23:40:33 -0500

    Update question10.py

--- question10.py
+++ question10.py
@@ -1,39 +1,49 @@
 def question10_1():
-    answer = True
+    answer = False
+    #Count data can be affected by noise, which may arise due to various factors such as measurement errors.
     return answer
 
 def question10_2():
-    answer = True    
+    answer = True
+    #A linear relationship between two sets of real values may be found with a correlation coefficient, which is a number between -1 and 1.
     return answer
 
 def question10_3():
-    answer = False    
+    answer = False
+    #Since aggregating time series data preserves more information about the trends and patterns in the data, it may be better than simple sampling.
     return answer
 
 def question10_4():
-    answer = False    
+    answer = False
+    #While there are some similarities between noise and outliers, outliers are extreme numbers that may or may not be the result of noise.
     return answer
 
 def question10_5():
-    answer = False    
+    answer = False
+    #An object being an outlier doesn't necessarily mean it is noise; it might be a legitimate extreme value in the data.
     return answer
 
 def question10_6():
-    answer = True    
+    answer = False
+    #An asymmetric binary attribute would have values between -1 and 1. A symmetric binary attribute has values between 0 and 1.
     return answer
 
 def question10_7():
-    answer = True    
+    answer = False
+    #A cosine measure of 1 between vectors of counts indicates the objects have the same direction but may not have the same magnitudes
     return answer
 
 def question10_8():
-    answer = False    
+    answer = False
+    #If a discrete variable, has a genuine zero point, then it can be a ratio
     return answer
 
 def question10_9():
-    answer = False    
+    answer = False
+    #Either discrete or continuous variables can be quantitative; continuity is not a requirement for being quantitative.
     return answer
 
 def question10_10():
-    answer = False    
+    answer = False
+    #Information on the ordinal relationships between categories may be lost when converting ordinal variables to asymmetric binary variables.
     return answer
